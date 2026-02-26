1b6c7068ddd8deb81d28fc07d40b091f7ca43173
edoespro <89481274+edoespro@users.noreply.github.com>
2026-02-26 00:13:08 -0600

    VirtualRepos: update GesPI/app/build.gradle.kts

--- GesPI/app/build.gradle.kts
+++ GesPI/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android") version "2.2.21"
-    
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 android {
