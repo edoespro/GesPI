2c2633729afce59099072f4e7621e2f950016a1b
edoespro <89481274+edoespro@users.noreply.github.com>
2026-02-25 20:41:42 -0600

    VirtualRepos: update GesPI/settings.gradle.kts

--- GesPI/settings.gradle.kts
+++ GesPI/settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
      repositoriesMode.set(
-    RepositoriesMode.FAIL_ON_PROJECT_REPOS) repositories {
+RepositoriesMode.FAIL_ON_PROJECT_REPOS) repositories {
              google()
               mavenCentral()
                }
